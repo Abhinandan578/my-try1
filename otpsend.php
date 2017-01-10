1e57a90667ce5cab3a81c31851c028bf59b01b04
piyush2897 <piyushyadav2897@gmail.com>
2017-01-10 22:01:05 +0530

    new optsend

--- otpsend.php
+++ otpsend.php
@@ -8,31 +8,15 @@ $query="SELECT OTP FROM OTP_Details WHERE Username='".$_SESSION['Username']."'";
 		$res1=mysqli_query($db, $query1);
 		$row1=mysqli_fetch_assoc($res1);
 		$Phone_No=$row1["Phone_No"];
-$post_data = array(
-    // 'From' doesn't matter; For transactional, this will be replaced with your SenderId;
-    // For promotional, this will be ignored by the SMS gateway
-    'From'   => '8607451509',
-    'To'    => $Phone_No,
-    'Body'  => 'Your OTP for login to SecureIt.com is " '.$otp.'"',
-);
- 
-$exotel_sid = "secureit"; // Your Exotel SID
-$exotel_token = "b40d460970c052ffe9c06f428b6c969ba8a91c4a"; // Your exotel token
- 
-$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Sms/send";
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+		$account_sid = 'AC9bd7bb940e244119f9207358564dc1fd';
+    $auth_token = '1be4ae20f1dc7c0ebac9c0211e15358d';
+    $client = new Services_Twilio($account_sid, $auth_token);
 
-	$http_result = curl_exec($ch);
-	$error = curl_error($ch);
-	$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE); 
-	curl_close($ch);
-	// print "Response = ".print_r($http_result);
-?>
+    $client->account->messages->create(
+        array(
+            'To' => '7206305374',
+            'From' => '72063053774',
+            'Body' => 'Your OTP for login to SecureIt.com is "'.$otp.'"',
+        )
+    );
+    ?>
