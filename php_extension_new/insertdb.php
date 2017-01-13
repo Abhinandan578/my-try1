b81773128458e0f891505890cd9eb4ff345235a1
abhinandan578 <abhinadan578@gmail.com>
2017-01-13 22:34:13 +0530

    Piyush  Open It

---
+++ php_extension_new/insertdb.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['url']))
+{
+	include("config.php");
+	$query="INSERT INTO URL_Details (URL) VALUES ('url')";
+	$res=mysqli_query($db,$query);
+	if($res)
+	{
+		echo "Query Succesful";
+	}
+	else
+	{
+		echo mysqli_error($db);
+	}
+	mysqli_close($db);
+
+}
+?>
